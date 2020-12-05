802b131afe5a264aea3731feaa19ebc8ea4f58a0
haakooto <hakonolav.torvik@gmail.com>
2020-12-05 14:16:58 +0100

    oneline function

--- 05/05.py
+++ 05/05.py
@@ -1,19 +1,11 @@
 import numpy as np
 
-def seatID(seat):
-    col = seat[-3:].replace("L", "0").replace("R", "1")
-    row = seat[:-3].replace("F", "0").replace("B", "1")
-    row = sum([int(i) * 2 ** j for j, i in enumerate(row[::-1])])
-    col = sum([int(i) * 2 ** j for j, i in enumerate(col[::-1])])
-    ID = row * 8 + col
-    return ID
-
 maxID = 0
 ids = np.arange(1033)
 seats = []
 with open("data.txt", "r") as file:
     for line in file.read().split("\n"):
-        seat = seatID(line)
+        seat = sum([int(i) * 2 ** j for j, i in enumerate(line.replace("L", "0").replace("R", "1").replace("F", "0").replace("B", "1")[::-1])])
         seats.append(seat)
         if seat > maxID:
             maxID = seat
