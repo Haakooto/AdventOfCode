e2ecb21bb4d797dd736c17f30271d0bf974fc4b5
haakooto <hakonolav.torvik@gmail.com>
2020-12-06 12:58:31 +0100

    move 2020 to own dir

--- 01/01.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-with open("data.txt", "r") as f:
-    d = np.array([int(i.strip()) for i in f.read().split()[:-1]])
-a, b, c = np.meshgrid(d, d, d)
-
-print(np.prod(d[np.where((a + b)[:, :, 0] == 2020)[0]]))
-print(np.prod(d[np.array(np.where(a + b + c == 2020))[:, 0]]))
