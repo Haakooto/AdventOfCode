a8b36ceefed12308ea5cc44f51fbbe15ea89e1aa
haakooto <hakonolav.torvik@gmail.com>
2020-12-09 08:20:04 +0100

    dag 9

---
+++ 2020/09/09.py
@@ -0,0 +1,16 @@
+import numpy as np
+from itertools import permutations
+nums = np.loadtxt("i", dtype=int, delimiter="\n")
+
+pream = 25
+idx = pream
+while nums[idx] in [sum(i) for i in list(permutations(nums[idx - pream: idx], 2))]:
+    idx += 1
+num1 = nums[idx]
+print(num1)
+
+for set_len in range(2, len(nums)):
+    for i in range(0, len(nums) - set_len):
+        cont = nums[i: i + set_len]
+        if sum(cont) == num1:
+            print(min(cont) + max(cont))
