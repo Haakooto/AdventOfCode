eacba3c15312389ed02a3dc89c7f8e572481a95c
haakooto <hakonolav.torvik@gmail.com>
2020-12-19 14:55:06 +0100

    day 17 a little late

---
+++ 2020/17/conway_cubes_3d.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+analogue = np.loadtxt("input", dtype=int, comments=None, converters={0: lambda s: [i for i in s]}, ndmin=2)
+
+active = []
+for y, col in enumerate(analogue):
+    for x, cube in enumerate(col):
+        if cube == 35:
+            active.append((x, y, 0))
+
+diff = np.arange(-1, 2)
+diffs = []
+for dx in diff:
+    for dy in diff:
+        for dz in diff:
+            if not (dx == dy == dz == 0):
+                diffs.append((dx, dy, dz))
+
+
+def game_of_cubes(active):
+    neighbours = {}
+    for cell in active:
+        if cell not in neighbours:
+            neighbours[cell] = 0
+        for neigh in get_neighs(*cell):
+            if neigh in neighbours:
+                neighbours[neigh] += 1
+            else:
+                neighbours[neigh] = 1
+
+    new = []
+    for cell, neighs in neighbours.items():
+        if cell in active:
+            if neighs in (2, 3):
+                new.append(cell)
+        else:
+            if neighs == 3:
+                new.append(cell)
+    return set(new)
+
+
+def get_neighs(x, y, z):
+    neighs = []
+    for dx, dy, dz in diffs:
+        neighs.append((x + dx, y + dy, z + dz))
+    return neighs
+
+
+for i in range(6):
+    active = game_of_cubes(active)
+print(len(active))
